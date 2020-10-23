b8cdae9e04341c58cf11f472104a64a0e26ede94
Toby Twigger <tobytwigger1@gmail.com>
2020-10-23 03:23:28 +0100

    Add supported languages supportg

---
+++ src/Translate/Interceptors/SupportedLanguageInterceptor.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Twigger\Translate\Translate\Interceptors;
+
+use Illuminate\Support\Arr;
+use Twigger\Translate\Translate\TranslationInterceptor;
+
+class SupportedLanguageInterceptor extends TranslationInterceptor
+{
+
+    /**
+     * Supported languages
+     *
+     * @var array
+     */
+    private static $supported = [];
+
+    /**
+     * We can interept if the language is NOT supported
+     *
+     * @inheritDoc
+     */
+    protected function canIntercept(string $line, string $to, string $from): bool
+    {
+        return !empty(static::$supported) && !in_array($to, static::$supported);
+    }
+
+    /**
+     * Return null to mark translation not supported
+     *
+     * @inheritDoc
+     */
+    protected function get(string $line, string $to, string $from): ?string
+    {
+        return null;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function save(string $line, string $to, string $from, string $translation): void
+    {
+        // No saving needed
+    }
+
+    /**
+     * Support a language or languages
+     *
+     * @param array $languages
+     */
+    public static function support($languages = []): void
+    {
+        static::$supported = array_merge(static::$supported, Arr::wrap($languages));
+    }
+
+    /**
+     * Revoke support of a language
+     *
+     * @param array|string $languages An array of languages or a single language
+     */
+    public static function revoke($languages = []): void
+    {
+        static::$supported = array_diff(static::$supported, Arr::wrap($languages));
+    }
+
+    /**
+     * Revoke all language support
+     */
+    public static function revokeAll(): void
+    {
+        static::$supported = [];
+    }
+}
---
+++ tests/Unit/Translate/Interceptors/SupportedLanguageInterceptorTest.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Twigger\Tests\Translate\Unit\Translate\Interceptors;
+
+use Prophecy\Argument;
+use Twigger\Tests\Translate\TestCase;
+use Twigger\Translate\Translate\Interceptors\SupportedLanguageInterceptor;
+
+class SupportedLanguageInterceptorTest extends TestCase
+{
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        SupportedLanguageInterceptor::revokeAll();
+    }
+
+    protected function tearDown(): void
+    {
+        SupportedLanguageInterceptor::revokeAll();
+    }
+
+    /** @test */
+    public function it_allows_a_language_if_supported_languages_empty(){
+        $translator = $this->prophesize(\Twigger\Translate\Translate\Translator::class);
+        $translator->translate(Argument::any(), Argument::any(), Argument::any())->shouldBeCalled()
+            ->willReturn('Test Line Translated');
+
+        $interceptor = new \Twigger\Translate\Translate\Interceptors\SupportedLanguageInterceptor(
+            [], $translator->reveal()
+        );
+        $this->assertEquals('Test Line Translated', $interceptor->translate('Test Line', 'fr', 'en'));
+    }
+
+    /** @test */
+    public function it_allows_a_target_language_if_in_supported_languages()
+    {
+        SupportedLanguageInterceptor::support(['en', 'fr', 'ru', 'en_US']);
+
+        $translator = $this->prophesize(\Twigger\Translate\Translate\Translator::class);
+        $translator->translate(Argument::any(), Argument::any(), Argument::any())->shouldBeCalled()
+            ->willReturn('Test Line Translated');
+
+        $interceptor = new \Twigger\Translate\Translate\Interceptors\SupportedLanguageInterceptor(
+            [], $translator->reveal()
+        );
+        $this->assertEquals('Test Line Translated', $interceptor->translate('Test Line', 'fr', 'en'));
+    }
+
+    /** @test */
+    public function it_allows_a_target_language_if_in_supported_languages_and_source_lang_not()
+    {
+        SupportedLanguageInterceptor::support(['en', 'fr', 'ru', 'en_US']);
+
+        $translator = $this->prophesize(\Twigger\Translate\Translate\Translator::class);
+        $translator->translate(Argument::any(), Argument::any(), Argument::any())->shouldBeCalled()
+            ->willReturn('Test Line Translated');
+
+        $interceptor = new \Twigger\Translate\Translate\Interceptors\SupportedLanguageInterceptor(
+            [], $translator->reveal()
+        );
+        $this->assertEquals('Test Line Translated', $interceptor->translate('Test Line', 'fr', 'en_GB'));
+    }
+
+    /** @test */
+    public function it_denies_a_language_if_source_language_not_in_supported_languages()
+    {
+        SupportedLanguageInterceptor::support(['en', 'ru', 'en_US']);
+
+        $translator = $this->prophesize(\Twigger\Translate\Translate\Translator::class);
+        $translator->translate(Argument::any(), Argument::any(), Argument::any())->shouldNotBeCalled();
+
+        $interceptor = new \Twigger\Translate\Translate\Interceptors\SupportedLanguageInterceptor(
+            [], $translator->reveal()
+        );
+
+        $this->assertNull($interceptor->translate('Test Line', 'fr', 'en'));
+    }
+
+    /** @test */
+    public function it_allows_multiple_calls_to_support()
+    {
+        SupportedLanguageInterceptor::support(['ru']);
+        SupportedLanguageInterceptor::support(['fr']);
+
+        $translator = $this->prophesize(\Twigger\Translate\Translate\Translator::class);
+        $translator->translate(Argument::any(), Argument::any(), Argument::any())->shouldBeCalled()
+            ->willReturn('Test Line Translated');
+
+        $interceptor = new \Twigger\Translate\Translate\Interceptors\SupportedLanguageInterceptor(
+            [], $translator->reveal()
+        );
+
+        $this->assertEquals('Test Line Translated', $interceptor->translate('Test Line', 'ru', 'en'));
+        $this->assertEquals('Test Line Translated', $interceptor->translate('Test Line', 'fr', 'en'));
+    }
+
+    /** @test */
+    public function it_allows_a_language_to_be_revoked()
+    {
+        SupportedLanguageInterceptor::support(['ru', 'fr']);
+        SupportedLanguageInterceptor::revoke(['fr']);
+
+        $translator = $this->prophesize(\Twigger\Translate\Translate\Translator::class);
+        $translator->translate(Argument::any(), Argument::any(), Argument::any())->shouldBeCalled()
+            ->willReturn('Test Line Translated');
+
+        $interceptor = new \Twigger\Translate\Translate\Interceptors\SupportedLanguageInterceptor(
+            [], $translator->reveal()
+        );
+
+        $this->assertEquals('Test Line Translated', $interceptor->translate('Test Line', 'ru', 'en'));
+        $this->assertNull($interceptor->translate('Test Line', 'fr', 'en'));
+    }
+
+    /** @test */
+    public function it_revokes_all_languages()
+    {
+        SupportedLanguageInterceptor::support(['ru', 'fr']);
+        SupportedLanguageInterceptor::revokeAll();
+        SupportedLanguageInterceptor::support(['en']);
+
+        $translator = $this->prophesize(\Twigger\Translate\Translate\Translator::class);
+        $translator->translate(Argument::any(), Argument::any(), Argument::any())->shouldBeCalled()
+            ->willReturn('Test Line Translated');
+
+        $interceptor = new \Twigger\Translate\Translate\Interceptors\SupportedLanguageInterceptor(
+            [], $translator->reveal()
+        );
+
+        $this->assertNull($interceptor->translate('Test Line', 'ru', 'en'));
+        $this->assertNull($interceptor->translate('Test Line', 'fr', 'en'));
+        $this->assertEquals('Test Line Translated', $interceptor->translate('Test Line', 'en', 'en'));
+    }
+
+}
